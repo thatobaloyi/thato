c9f11e970f23b225a67802786244e8708db61511
Thato Baloyi <thato2313321@gmail.com>
2023-12-13 13:48:22 +0200

    'added'

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express();
 const dotenv = require('dotenv').config()
 const cors = require('cors')
@@ -11,6 +12,19 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors())
 
 app.use('/projects', require('./routers/projectsRouter'))
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+    app.get('*', (req, res)=>{
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
+        )
+    })
+}else{
+    app.get('/', (req,res)=>{
+        res.send("Please set to production")
+    })
+}
 
 app.use(errorHandler)
 app.listen(port, console.log(`Server Running on port ${port}`))
