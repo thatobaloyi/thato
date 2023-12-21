63f07925115e38f7613ae7b778ecf0f3641f6424
Thato Baloyi <thato2313321@gmail.com>
2023-12-21 20:47:59 +0200

    links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Socials2 from './Socials2';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll'
 
 function Header() {
+  const location = useLocation()
+  const isHomePage = location.pathname === '/'
 
   return (
     <header id='header'>
@@ -18,10 +20,10 @@ function Header() {
               <RouterLink to='/faq'><li>FAQ</li></RouterLink>
             </ul>
           </li>
-          <li><ScrollLink to='about-me' smooth={true} duration={500} offset={-200}>About</ScrollLink></li>
-          <li><ScrollLink to='skills' smooth={true} duration={500} offset={-180}>Skills</ScrollLink></li>
-          <li><ScrollLink to='experience' smooth={true} duration={500} offset={-180}>Experience</ScrollLink></li>
-          <li><ScrollLink to='education' smooth={true} duration={500} offset={-200}>Education</ScrollLink></li>
+          {isHomePage && <li><ScrollLink to='about-me' smooth={true} duration={500} offset={-200}>About</ScrollLink></li>}
+          {isHomePage && <li><ScrollLink to='skills' smooth={true} duration={500} offset={-180}>Skills</ScrollLink></li>}
+          {isHomePage && <li><ScrollLink to='experience' smooth={true} duration={500} offset={-180}>Experience</ScrollLink></li>}
+          {isHomePage &&<li><ScrollLink to='education' smooth={true} duration={500} offset={-200}>Education</ScrollLink></li>}
         </ul>
       </div>
       <h1><RouterLink to='/'>Thato Emmanuel Baloyi</RouterLink></h1>
