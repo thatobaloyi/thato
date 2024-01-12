39fd1270c539e78b3b68c487bfca879d0d1e5adf
Thato Baloyi <102561962+thatobaloyi@users.noreply.github.com>
2024-01-12 22:42:56 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,7 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors({
-    origin: ["https://thato-portfolio-frontend.vercel.app/"],
-    methods:["GET", "POST"],
-    credentials: true
-}))
+app.use(cors())
 app.use('/projects', require('./routers/projectsRouter'))
 
 if(process.env.NODE_ENV === 'production'){
