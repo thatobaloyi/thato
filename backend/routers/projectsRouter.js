f1f3d43bbdbc21570a989fa9bbb51841a69684a6
Thato Baloyi <102561962+thatobaloyi@users.noreply.github.com>
2024-01-13 00:35:34 +0200

    Update projectsRouter.js

--- backend/routers/projectsRouter.js
+++ backend/routers/projectsRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getProjects } = require('../controllers/projectsController')
 
-router.get('/api/', getProjects)
+router.get('/', getProjects)
 
 
 module.exports = router
