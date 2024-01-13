57cfb24f4221bf0e9af4631feef335df3bada6b9
Thato Baloyi <102561962+thatobaloyi@users.noreply.github.com>
2024-01-13 02:31:35 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://thato-frontend.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://thato-frontend.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
