f1bc6d87439e17d3fd125f63f35a7a0c380e05f9
Thato Baloyi <102561962+thatobaloyi@users.noreply.github.com>
2024-01-13 21:31:43 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use(cors())
-app.use('/api/projects', require('./routers/projectsRouter'))
+app.use('/projects', require('./routers/projectsRouter'))
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'../frontend/dist')))
