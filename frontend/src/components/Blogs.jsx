34ee55944e622c392c20ba77ed54d5ec6e33847a
Thato Baloyi <thato2313321@gmail.com>
2024-01-19 14:24:46 +0200

    updated

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -10,18 +10,6 @@ function Blogs() {
           <h1><Link to='/coding/react-guide'>Mastering React: A Comprehensive Guide for Beginners</Link></h1>
           <p>Welcome to the exciting world of React! Whether you're a newcomer to web development or looking to enhance your skills, React is a powerful JavaScript library that can help you build dynamic and interactive user interfaces. In this comprehensive guide, we'll take you through the fundamentals of React, from the basics to more advanced concepts, to empower you on your journey to becoming a React expert.</p>
         </div>
-        <div className="blog">
-          <h1><Link to='/coding/react-guide'>Mastering React: A Comprehensive Guide for Beginners</Link></h1>
-          <p>Welcome to the exciting world of React! Whether you're a newcomer to web development or looking to enhance your skills, React is a powerful JavaScript library that can help you build dynamic and interactive user interfaces. In this comprehensive guide, we'll take you through the fundamentals of React, from the basics to more advanced concepts, to empower you on your journey to becoming a React expert.</p>
-        </div>
-        <div className="blog">
-          <h1><Link to='/coding/react-guide'>Mastering React: A Comprehensive Guide for Beginners</Link></h1>
-          <p>Welcome to the exciting world of React! Whether you're a newcomer to web development or looking to enhance your skills, React is a powerful JavaScript library that can help you build dynamic and interactive user interfaces. In this comprehensive guide, we'll take you through the fundamentals of React, from the basics to more advanced concepts, to empower you on your journey to becoming a React expert.</p>
-        </div>
-        <div className="blog">
-          <h1><Link to='/coding/react-guide'>Mastering React: A Comprehensive Guide for Beginners</Link></h1>
-          <p>Welcome to the exciting world of React! Whether you're a newcomer to web development or looking to enhance your skills, React is a powerful JavaScript library that can help you build dynamic and interactive user interfaces. In this comprehensive guide, we'll take you through the fundamentals of React, from the basics to more advanced concepts, to empower you on your journey to becoming a React expert.</p>
-        </div>
       </div>
     </>
   )
