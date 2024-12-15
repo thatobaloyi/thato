d1688e946a46b5d385c4a784d41e7e1723c587ed
Thato Baloyi <thato2313321@gmail.com>
2024-12-15 22:07:23 +0200

    added target attribute to links

--- frontend/src/components/AboutMe.jsx
+++ frontend/src/components/AboutMe.jsx
@@ -7,7 +7,7 @@ function AboutMe() {
       <h1>About Me</h1>
       <center><hr /></center>
         {/* <Image></Image> */}
-        <p>I'm Thato Emmanuel Baloyi, a Computer Science major at <a href="https://www.ru.ac.za/">Rhodes University.</a> A Highly motivated second-year BSc Computer Science and Information Systems student with a strong passion for full-stack development and IT specialization. Achieved first place in my first-year computer science class, demonstrating a solid foundation in programming concepts and problem-solving skills. I am a dedicated learner, continuously seeking to expand my knowledge of the latest technological advancements and their practical applications.</p>
+        <p>I'm Thato Emmanuel Baloyi, a Computer Science major at <a href="https://www.ru.ac.za/" target='_blank'>Rhodes University.</a> A Highly motivated second-year BSc Computer Science and Information Systems student with a strong passion for full-stack development and IT specialization. Achieved first place in my first-year computer science class, demonstrating a solid foundation in programming concepts and problem-solving skills. I am a dedicated learner, continuously seeking to expand my knowledge of the latest technological advancements and their practical applications.</p>
     </div>
   )
 }
